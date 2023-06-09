df361df90eb3cd111df34346c3eccd4fc484b787
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-06-09 12:08:08 -0300

    Fixes no TrainerPokemonController

--- src/lib/ex_mon_web/controllers/trainer_pokemons_controller.ex
+++ src/lib/ex_mon_web/controllers/trainer_pokemons_controller.ex
@@ -7,10 +7,10 @@ defmodule ExMonWeb.TrainersController do
     |> handle_response(conn, "create.json", :created)
   end
 
-  defp handle_response({:ok, trainer}, conn, view, status) do
+  defp handle_response({:ok, pokemon}, conn, view, status) do
     conn
     |> put_status(status)
-    |> render(view, trainer: trainer)
+    |> render(view, pokemon: pokemon)
   end
 
   defp handle_response({:error, _changeset} = error, _conn, _view, _status), do: error
